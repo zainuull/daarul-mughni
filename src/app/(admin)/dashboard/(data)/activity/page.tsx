dc64d4f9940ef895d9f3fc59b70e69dc5590a703
Zainul <thobazainul@gmail.com>
2023-11-19 16:49:53 +0700

    fix: bug

--- src/app/(admin)/dashboard/(data)/activity/page.tsx
+++ src/app/(admin)/dashboard/(data)/activity/page.tsx
@@ -7,18 +7,18 @@ const DataActivity = () => {
   return (
     <div className="w-full">
       <div className="p-10 w-full h-full flex flex-col gap-y-4 ">
-        <h1 className="text-2xl uppercase font-medium">Laporan Kegiatan</h1>
+        <h1 className="text-2xl uppercase font-medium">Laporan Acara</h1>
         <div className="w-full flex items-center justify-between mt-2">
-          <h2 className="font-light uppercase">Daftar Kegiatan</h2>
+          <h2 className="font-light uppercase">Daftar Acara</h2>
           <Link
             href={'/dashboard/activity/add-data-activity'}
             className="px-6 py-2 bg-primary rounded-lg text-slate-700">
-            Tambah Data Kegiatan
+            Tambah Data Acara
           </Link>
         </div>
         <div className="w-full flex items-center gap-x-4 px-4 py-3 rounded-xl border border-primary">
           <BsSearch />
-          <input className="w-full outline-none" placeholder="Cari Data Kegiatan" />
+          <input className="w-full outline-none" placeholder="Cari Data Acara" />
         </div>
         <TableList />
       </div>
